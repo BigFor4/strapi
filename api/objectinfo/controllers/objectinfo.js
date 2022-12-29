4b107c39f8640b1f1dd5782e70efc741336a38bc
dangnh1 <dangnh1.bhsoft@gmail.com>
2022-12-29 15:38:51 +0700

    UPDATE

--- api/objectinfo/controllers/objectinfo.js
+++ api/objectinfo/controllers/objectinfo.js
@@ -1,31 +1,177 @@
 'use strict';
-const {
-  Types: { ObjectId: ObjectId },
-} = require("mongoose");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  async getTree() {
-    let id =  ObjectId('ffffffffffffffffffffffff')
-    const rootNodes = await strapi.query('objectinfo').model.find({parent : id}).lean().exec();
-
-    const tree = await this.getTreeRecursive(rootNodes);
-
-    return tree;
+  async getTree(ctx) {
+    const { fileid } = ctx.params
+    const rootNodes = await strapi.query('objectinfo').model.aggregate([
+      {
+        $match: {
+          $or: [
+            { fileid },
+            { parent: "ffffffffffffffffffffffff" }
+          ]
+        }
+      },
+      {
+        $graphLookup: {
+          from: "datas",
+          startWith: "$_id",
+          connectFromField: "_id",
+          connectToField: "parent",
+          depthField: "level",
+          as: "children"
+        }
+      },
+      {
+        $unwind: {
+          path: "$children",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $sort: {
+          "children.level": -1
+        }
+      },
+      {
+        $group: {
+          _id: "$_id",
+          parent: {
+            $first: "$parent"
+          },
+          name: {
+            $first: "$name"
+          },
+          type: {
+            $first: 1
+          },
+          fileid: {
+            $first: "$fileid"
+          },
+          projectid: {
+            $first: "$projectid"
+          },
+          inf: {
+            $first: "$inf"
+          },
+          children: {
+            $push: "$children"
+          }
+        }
+      },
+      {
+        $addFields: {
+          children: {
+            $reduce: {
+              input: "$children",
+              initialValue: {
+                level: -1,
+                presentChild: [],
+                prevChild: []
+              },
+              in: {
+                $let: {
+                  vars: {
+                    prev: {
+                      $cond: [
+                        {
+                          $eq: [
+                            "$$value.level",
+                            "$$this.level"
+                          ]
+                        },
+                        "$$value.prevChild",
+                        "$$value.presentChild"
+                      ]
+                    },
+                    current: {
+                      $cond: [
+                        {
+                          $eq: [
+                            "$$value.level",
+                            "$$this.level"
+                          ]
+                        },
+                        "$$value.presentChild",
+                        []
+                      ]
+                    }
+                  },
+                  in: {
+                    level: "$$this.level",
+                    prevChild: "$$prev",
+                    presentChild: {
+                      $concatArrays: [
+                        "$$current",
+                        [
+                          {
+                            $mergeObjects: [
+                              "$$this",
+                              {
+                                children: {
+                                  $filter: {
+                                    input: "$$prev",
+                                    as: "e",
+                                    cond: {
+                                      $eq: [
+                                        "$$e.parent",
+                                        "$$this._id"
+                                      ]
+                                    }
+                                  }
+                                }
+                              }
+                            ]
+                          }
+                        ]
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        $addFields: {
+          id: "$_id",
+          children: "$children.presentChild"
+        }
+      }
+    ]).lean().exec();
+    return rootNodes;
   },
-  async getTreeRecursive(nodes) {
+  async getTreeRecursive(nodes, fileid, count) {
     const tree = [];
     for (const node of nodes) {
-      let data  =  node._id
-      const children = await strapi.query('objectinfo').model.find({ parent: data }).lean()
-        .exec();
-      node.children = await this.getTreeRecursive(children);
+      const children = await strapi.query('objectinfo').model.find({ parent: node._id , fileid }).lean().exec();
+      if (children && children.length > 0 && count > 0) {
+        node.children = await this.getTreeRecursive(children, fileid, count - 1);
+      }
       tree.push(node);
     }
-
     return tree;
-  }
+  },
+  // async getTree(ctx) {
+  //   const { fileid } = ctx.params
+  //   const rootNodes = await strapi.query('objectinfo').model.find({ parent: "ffffffffffffffffffffffff" , fileid }).lean().exec();
+  //   const tree = await this.getTreeRecursive(rootNodes, fileid);
+  //   return tree;
+  // },
+  // async getTreeRecursive(nodes, fileid) {
+  //   const tree = [];
+  //   for (const node of nodes) {
+  //     const children = await strapi.query('objectinfo').model.find({ parent: node._id , fileid}).lean().exec();
+  //     // if (children && children.length > 0 && count > 0) {
+  //       node.children = await this.getTreeRecursive(children, fileid);
+  //     // }
+  //     tree.push(node);
+  //   }
+  //   return tree;
+  // }
 };
--- config/database.js
+++ config/database.js
@@ -4,11 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST', 'localhost'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'local'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
-});
+})
