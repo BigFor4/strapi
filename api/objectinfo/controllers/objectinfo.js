86f06d2586f94935e81ef90640909cddd22efd04
dangnh1 <dangnh1.bhsoft@gmail.com>
2023-01-09 09:13:01 +0700

    update

--- api/objectinfo/controllers/objectinfo.js
+++ api/objectinfo/controllers/objectinfo.js
@@ -6,9 +6,11 @@
 const {
   Types: { ObjectId: ObjectId },
 } = require("mongoose");
+const tree = require("./../../../tree")
 module.exports = {
   async getTreeObjectInfo(ctx) {
     const { fileid } = ctx.params
+    let modelType = "ifc"
     let parent = ObjectId('ffffffffffffffffffffffff')
     const rootNodes = await strapi.query('objectinfo').model.aggregate([
       {
@@ -32,7 +34,7 @@ module.exports = {
           connectToField: "parent",
           depthField: "level",
           as: "children",
-          maxDepth: 6
+          maxDepth: 100
         }
       },
       {
@@ -45,7 +47,9 @@ module.exports = {
                 _id: '$$child._id',
                 key: '$$child._id',
                 parent: '$$child.parent',
-                title: '$$child.name',
+                title: '$$child.inf',
+                GUID: '$$child.guid',
+                inf: '$$child.inf',
                 type: '$$child.type',
                 fileid: '$$child.fileid',
                 projectid: '$$child.projectid',
@@ -76,13 +80,16 @@ module.exports = {
             $first: '$_id',
           },
           title: {
-            $first: '$name',
+            $first: '$inf',
           },
           type: {
             $first: '$type',
           },
-          guid: {
+          GUID: {
             $first: '$guid',
+          },
+          inf: {
+            $first: '$inf',
           },
           fileid: {
             $first: '$fileid',
@@ -176,247 +183,233 @@ module.exports = {
         }
       }
     ]);
-    return rootNodes;
+    let data = tree.shortenTree(modelType, [...rootNodes], modelType === "ifc" ? "IFCReader 2.0" : "LandXmlReader 2.0", [], 0, "")
+    return data;
   },
   async getTreeGuid(ctx) {
-    const { guid, fileid } = ctx.request.body
-    const rootNodes = await strapi.query('objectinfo').model.aggregate([
-      {
-        $match: {
-          $and: [
-            { fileid },
-            { guid }
-          ]
-        }
-      },
-      {
-        $lookup: {
-          from: 'datas',
-          localField: 'parent',
-          foreignField: '_id',
-          as: 'parent_level_1',
-        },
-      },
-      {
-        $lookup: {
-          from: 'datas',
-          localField: 'parent_level_1.parent',
-          foreignField: '_id',
-          as: 'parent_level_2',
-        },
-      },
-      {
-        $project: {
-          _id: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2._id', []],
-              },
-              '$_id',
-              { $arrayElemAt: ['$parent_level_2._id', 0] },
-            ],
-          },
-          guid: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2.guid', []],
-              },
-              '$guid',
-              { $arrayElemAt: ['$parent_level_2.guid', 0] },
-            ],
-          },
-          title: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2.name', []],
-              },
-              '$name',
-              { $arrayElemAt: ['$parent_level_2.name', 0] },
-            ],
-          },
-          fileid: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2.fileid', []],
-              },
-              '$fileid',
-              { $arrayElemAt: ['$parent_level_2.fileid', 0] },
-            ],
-          },
-          projectid: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2.projectid', []],
-              },
-              '$projectid',
-              { $arrayElemAt: ['$parent_level_2.projectid', 0] },
-            ],
-          },
-          type: {
-            $cond: [
-              {
-                $eq: ['$parent_level_2.type', []],
-              },
-              '$type',
-              { $arrayElemAt: ['$parent_level_2.type', 0] },
-            ],
-          },
-          parent: {
-            $cond: [
-              {
-                $ne: ['$parent_level_2.parent', []],
-              },
-              '$parent',
-              { $arrayElemAt: ['$parent_level_2.parent', 0] },
-            ],
-          },
-        },
-      },
-      {
-        $addFields: {
-          key: '$_id',
-        }
-      },
-      {
-        $graphLookup: {
-          from: 'datas',
-          startWith: '$_id',
-          connectFromField: '_id',
-          connectToField: 'parent',
-          depthField: 'level',
-          as: 'children',
-          maxDepth: 6,
-        },
-      },
-      {
-        $addFields: {
-          children: {
-            $map: {
-              input: '$children',
-              as: 'child',
-              in: {
-                _id: '$$child._id',
-                key: '$$child._id',
-                parent: '$$child.parent',
-                title: '$$child.name',
-                type: '$$child.type',
-                fileid: '$$child.fileid',
-                projectid: '$$child.projectid',
-                level: '$$child.level',
-              },
+    const { guids, fileid } = ctx.request.body;
+    let rootNodes = [];
+    if (guids?.length > 0) {
+      await Promise.all(guids.map(async guid => {
+        let rootNode = await strapi.query('objectinfo').model.aggregate([
+          {
+            $match: {
+              $and: [{ fileid }, { guid }],
             },
           },
-        },
-      },
-      {
-        $unwind: {
-          path: '$children',
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $sort: {
-          'children.level': -1,
-        },
-      },
-      {
-        $group: {
-          _id: '$_id',
-          parent: {
-            $first: '$parent',
+          {
+            $lookup: {
+              from: 'datas',
+              localField: '_id',
+              foreignField: '_id',
+              as: 'data',
+            },
           },
-          key: {
-            $first: '$_id',
-          },
-          title: {
-            $first: '$title',
-          },
-          type: {
-            $first: '$type',
-          },
-          fileid: {
-            $first: '$fileid',
-          },
-          projectid: {
-            $first: '$projectid',
-          },
-          children: {
-            $push: '$children',
-          },
-        },
-      },
-      {
-        $addFields: {
-          children: {
-            $reduce: {
-              input: '$children',
-              initialValue: {
-                level: -1,
-                presentChild: [],
-                prevChild: [],
-              },
-              in: {
-                $let: {
-                  vars: {
-                    prev: {
-                      $cond: [
-                        {
-                          $eq: ['$$value.level', '$$this.level'],
-                        },
-                        '$$value.prevChild',
-                        '$$value.presentChild',
-                      ],
-                    },
-                    current: {
-                      $cond: [
-                        {
-                          $eq: ['$$value.level', '$$this.level'],
-                        },
-                        '$$value.presentChild',
-                        [],
-                      ],
-                    },
+          {
+            $project: {
+              _id: { $arrayElemAt: ['$data._id', 0] },
+              GUID: { $arrayElemAt: ['$data.guid', 0] },
+              title: {
+                $cond: {
+                  if: {
+                    $eq: ['$data.name', [null]],
                   },
+                  then: { $arrayElemAt: ['$data.inf', 0] },
+                  else: { $arrayElemAt: ['$data.name', 0] },
+                },
+              },
+              fileid: { $arrayElemAt: ['$data.fileid', 0] },
+              inf: { $arrayElemAt: ['$data.inf', 0] },
+              projectid: { $arrayElemAt: ['$data.projectid', 0] },
+              type: { $arrayElemAt: ['$data.type', 0] },
+              parent: { $arrayElemAt: ['$data.parent', 0] },
+            },
+          },
+          {
+            $addFields: {
+              key: '$_id',
+            },
+          },
+          {
+            $graphLookup: {
+              from: 'datas',
+              startWith: '$_id',
+              connectFromField: '_id',
+              connectToField: 'parent',
+              depthField: 'level',
+              as: 'children',
+              maxDepth: 3,
+            },
+          },
+          {
+            $addFields: {
+              children: {
+                $map: {
+                  input: '$children',
+                  as: 'child',
                   in: {
-                    level: '$$this.level',
-                    prevChild: '$$prev',
-                    presentChild: {
-                      $concatArrays: [
-                        '$$current',
-                        [
-                          {
-                            $mergeObjects: [
-                              '$$this',
-                              {
-                                children: {
-                                  $filter: {
-                                    input: '$$prev',
-                                    as: 'e',
-                                    cond: {
-                                      $eq: ['$$e.parent', '$$this._id'],
-                                    },
-                                  },
-                                },
-                              },
-                            ],
-                          },
-                        ],
+                    _id: '$$child._id',
+                    key: '$$child._id',
+                    GUID: '$$child.guid',
+                    node: '$$child._id',
+                    parent: '$$child.parent',
+                    title: {
+                      $cond: [
+                        {
+                          $eq: ['$$child.name', null],
+                        },
+                        '$$child.inf',
+                        '$$child.name',
                       ],
                     },
+                    Name: {
+                      $cond: [
+                        {
+                          $eq: ['$$child.name', null],
+                        },
+                        '$$child.inf',
+                        '$$child.name',
+                      ],
+                    },
+                    inf: '$$child.inf',
+                    type: '$$child.type',
+                    fileid: '$$child.fileid',
+                    projectid: '$$child.projectid',
+                    level: '$$child.level',
                   },
                 },
               },
             },
           },
-        },
-      },
-      {
-        $addFields: {
-          id: '$_id',
-          children: '$children.presentChild',
-        },
-      },
-    ]);
-    return rootNodes;
+          {
+            $unwind: {
+              path: "$children",
+              preserveNullAndEmptyArrays: true
+            }
+          },
+          {
+            $sort: {
+              "children.level": -1
+            }
+          },
+          {
+            $group: {
+              _id: '$_id',
+              parent: {
+                $first: '$parent',
+              },
+              key: {
+                $first: '$_id',
+              },
+              node: {
+                $first: '$_id',
+              },
+              title: {
+                $first: '$title',
+              },
+              type: {
+                $first: '$type',
+              },
+              GUID: {
+                $first: '$GUID',
+              },
+              fileid: {
+                $first: '$fileid',
+              },
+              projectid: {
+                $first: '$projectid',
+              },
+              children: {
+                $push: '$children',
+              },
+            },
+          },
+          {
+            $addFields: {
+              children: {
+                $reduce: {
+                  input: "$children",
+                  initialValue: {
+                    level: -1,
+                    presentChild: [],
+                    prevChild: []
+                  },
+                  in: {
+                    $let: {
+                      vars: {
+                        prev: {
+                          $cond: [
+                            {
+                              $eq: [
+                                "$$value.level",
+                                "$$this.level"
+                              ]
+                            },
+                            "$$value.prevChild",
+                            "$$value.presentChild"
+                          ]
+                        },
+                        current: {
+                          $cond: [
+                            {
+                              $eq: [
+                                "$$value.level",
+                                "$$this.level"
+                              ]
+                            },
+                            "$$value.presentChild",
+                            []
+                          ]
+                        }
+                      },
+                      in: {
+                        level: "$$this.level",
+                        prevChild: "$$prev",
+                        presentChild: {
+                          $concatArrays: [
+                            "$$current",
+                            [
+                              {
+                                $mergeObjects: [
+                                  "$$this",
+                                  {
+                                    children: {
+                                      $filter: {
+                                        input: "$$prev",
+                                        as: "e",
+                                        cond: {
+                                          $eq: [
+                                            "$$e.parent",
+                                            "$$this._id"
+                                          ]
+                                        }
+                                      }
+                                    }
+                                  }
+                                ]
+                              }
+                            ]
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          {
+            $addFields: {
+              id: "$_id",
+              children: "$children.presentChild"
+            }
+          }
+        ]);
+        if(rootNode[0]){
+          rootNodes.push(rootNode[0])
+        }
+      }))
+    }
+    return rootNodes
   }
 };
---
+++ tree.js
@@ -0,0 +1,346 @@
+const { v4: uuidv4 } = require('uuid');
+
+const META_DATA_TYPES = {
+  IFC_PROPERTIES: 10,
+  IFC_HEADER_INFO: 13,
+  IFC_SHOW_TREE: 7,
+  LANDXML_ATTRIBUTES: 5,
+  LANDXML_META: 6,
+  LANDXML_FEATURE: 10,
+}
+
+const META_DATA_KEYS = {
+  DEFINITION: 'Definition',
+  FileSchemas: 'Set of FileSchemas'
+}
+
+const LANDXML_OBJ = {
+  PIPENETWORKS: 'PipeNetworks',
+  SURFACES: 'Surfaces',
+  CGPOINTS: 'CgPoints',
+  Alignments: 'Alignments',
+  PlanFeatures: 'PlanFeatures',
+  Project: 'Project',
+  CoordinateSystem: 'CoordinateSystem'
+}
+
+const LANDXML_KEY = {
+  UNITS: 'Units',
+  FeatureDictionary: 'FeatureDictionary',
+}
+
+function searchTreeNode(data, key, match) {
+  const stack = [];
+  data.map(item => stack.push(item));
+  while (stack.length > 0) {
+    const node = stack.pop();
+    if (node[key] === match) {
+      return node;
+    } else if (node.children) {
+      node.children.map(child => stack.push(child))
+    }
+  }
+  return null;
+}
+
+function searchTreeNodeForTowParents(data, key, match) {
+  const nodes = []
+  let node = searchTreeNodeForParent(data, key, match)
+  while (node) {
+    if(nodes.length < 2)
+    nodes.push(node.GUID)
+  }
+  return nodes
+}
+function searchTreeNodeForParent(data, key, match) {
+  const stack = [];
+
+  function compareArr(n = [], k, d) {
+    n.forEach((v, i) => {
+      if (v[k] === d) {
+        return n;
+      }
+    });
+  }
+  data.map(item => stack.push(item));
+  while (stack.length > 0) {
+    const node = stack.pop();
+    if (node[key] === match) {
+      return node.parrent;
+    }
+    if (node.children && node.children.length) {
+      if (compareArr(node.children, key, match)) {
+        return node;
+      }
+      else {
+        node.children.forEach(child => {
+          child.parrent = node
+          stack.push(child)
+        })
+      }
+    }
+  }
+  return null;
+}
+
+function traversalOTree(data) {
+  return data.map(item => {
+    if (item.children) {
+      const title = item.title ? item.title : setTitleRoot(item.children[0].type)
+      return {
+        ...item,
+        title,
+        key: item.node ? item.node : uuidv4(),
+        children: traversalOTree(item.children),
+      };
+    }
+    return {
+      ...item,
+      title: item.title,
+      key: item.node ? item.node : uuidv4(),
+    };
+  });
+}
+
+function setTitleRoot(type) {
+  switch (type) {
+    case 2:
+    case 8:
+      return "Not referenced"
+    case 6:
+      return "Space Boundaries"
+    case 7:
+    case 9:
+      return "IfcProject"
+    case 13:
+      return "Header Info"
+    default:
+      return ""
+  }
+}
+
+function shortenTree(modelType, data, version, attrData, level = 0, rootUnits) {
+  if (modelType === 'landxml') {
+    data.map((item) => {
+
+      let units = null;
+      if (item.children) {
+        units = item.children.find(child => child.title === LANDXML_KEY.UNITS);
+      }
+      else {
+
+      }
+      if (level === 0 && item.children && units) {
+        let FeatureDictionary = item.children.find(child => child.title === LANDXML_KEY.FeatureDictionary);
+
+        rootUnits = buildUnitsObject(units.children[0].children[0].children);
+        if(FeatureDictionary && FeatureDictionary.children.length > 0){
+          rootFeatureDictionary = buildUnitsObject(FeatureDictionary.children[0].children);
+          if (rootFeatureDictionary.version) {
+            version.name = rootFeatureDictionary.name + ' ' + rootFeatureDictionary.version
+          }
+        }
+
+        //rootUnits = {...rootUnits, ...rootFeatureDictionary}
+        item.children = item.children.filter(item => Object.values(LANDXML_OBJ).includes(item.title))
+      }
+      const unitMapper = units ? buildUnitsObject(units.children[0].children[0].children) : rootUnits;
+
+      if (item.children) {
+        const attributes = item.children.find(child => child.type === META_DATA_TYPES.LANDXML_ATTRIBUTES || child.type === META_DATA_TYPES.LANDXML_META);
+        const features = item.children.filter(child => child.type === META_DATA_TYPES.LANDXML_FEATURE);
+        const definition = item.children.find(child => child.title === META_DATA_KEYS.DEFINITION);
+        if(definition && !definition.GUID){
+          definition.GUID = uuidv4()
+        }
+        const newChildren = item.children.filter(child => child.type !== META_DATA_TYPES.LANDXML_ATTRIBUTES && child.type !== META_DATA_TYPES.LANDXML_FEATURE && child.title !== META_DATA_KEYS.DEFINITION);
+        if (!item.GUID) {
+          item.GUID = uuidv4()
+        }
+        if (definition) {
+          item.GUID = definition.GUID;
+        }
+        if (features && features.length) {
+          const childAttr = groupChildAttr(features);
+          if (attributes && attributes.children && childAttr && childAttr.length) {
+            attributes.children = [...attributes.children, ...childAttr];
+          } else if (attributes && childAttr && childAttr.length) {
+            attributes.children = childAttr;
+          }
+        }
+        if (attributes && attributes.children && attributes.children.length) {
+          if (definition) {
+            const definitionAttributes = definition.children.find(child => child.type === META_DATA_TYPES.LANDXML_ATTRIBUTES);
+            if (definitionAttributes && definitionAttributes.children && definitionAttributes.children.length) {
+              definitionAttributes.children.map(definitionAttribute => attributes.children.push(definitionAttribute));
+            }
+          }
+          const nameAttr = attributes.children.find(o => o.title.startsWith('name='));
+          if (nameAttr) {
+            item.title = `${item.title} '${nameAttr.title.split('=')[1]}'`;
+          }
+          buildAttrUnit(unitMapper, attributes.children);
+        }
+
+
+        attrData.push({
+          ...attributes,
+          pKey: item.key,
+          pTitle: item.title,
+          GUID: item.GUID,
+          version
+        });
+        item.children = newChildren;
+        item.isLeaf = !newChildren.length;
+
+        if (item.children && item.children.length) {
+          shortenTree(modelType, item.children, version, attrData, level + 1, rootUnits)
+        } else {
+          delete item.children;
+        }
+      } else {
+
+        item.isLeaf = true
+      }
+    })
+  } else if (modelType === 'ifc') {
+    data.map(item => {
+      const properties = item.children && item.children.find(child => child.type === META_DATA_TYPES.IFC_PROPERTIES);
+      if (properties && properties.children) {
+        properties.children.unshift({
+          key: uuidv4(),
+          title: `GlobalID = ${properties.GUID || ''}`,
+        })
+        item.GUID = properties.GUID
+      } else if (properties) {
+        properties.children = [{
+          key: uuidv4(),
+          title: `GlobalID = ${properties.GUID || ''}`,
+        }]
+        item.GUID = properties.GUID
+      }
+      if (item.children && item.children[0] && item.children[0].type === META_DATA_TYPES.IFC_HEADER_INFO) {
+        const fileSchemas = item.children.find(x => x.title === META_DATA_KEYS.FileSchemas)
+        if (fileSchemas && fileSchemas.children) {
+          const ifcversion = buildUnitsObject(fileSchemas.children)
+          if (ifcversion && Object.keys(ifcversion).length) {
+            version.name = ifcversion[Object.keys(ifcversion)[0]]
+          }
+        }
+        attrData.push({
+          ...item,
+          pKey: item.key,
+          pTitle: item.title,
+          version
+        });
+      } else {
+        attrData.push({
+          ...properties,
+          pKey: item.key,
+          pTitle: item.title,
+          version
+        });
+      }
+      if (item.children) {
+        const newChildren = [];
+        item.children.map(child => {
+          if (child.type === META_DATA_TYPES.IFC_SHOW_TREE) {
+            newChildren.push(child);
+          } else {
+            if (child.children) {
+              child.children.map(gchild => {
+                if (gchild.type === META_DATA_TYPES.IFC_SHOW_TREE) {
+                  newChildren.push(gchild);
+                }
+              })
+            }
+          }
+        })
+        item.children = newChildren;
+        item.isLeaf = !newChildren.length
+        shortenTree(modelType, item.children, version, attrData, level + 1)
+      } else {
+        item.isLeaf = true
+      }
+    })
+  }
+  return {
+    data,
+    attrData
+  };
+}
+
+function groupChildAttr(data) {
+  data.map(item => {
+    if (item.children) {
+      const attributes = item.children.find(child => child.type === META_DATA_TYPES.LANDXML_ATTRIBUTES);
+      const newChildren = item.children.filter(child => child.type !== META_DATA_TYPES.LANDXML_ATTRIBUTES);
+      if (item.type === META_DATA_TYPES.LANDXML_FEATURE) {
+        item.title = `${attributes.children[0] ? attributes.children[0].title.split('=')[1] : ''} '${attributes.children[1]
+          ? attributes.children[1].title.split('=')[1] : ''}'`;
+      } else {
+        item.title = `${attributes.children[0] ? attributes.children[0].title.split('=')[1] : ''}=${attributes.children[1]
+          ? attributes.children[1].title.split('=')[1] : ''}`;
+      }
+      item.children = newChildren;
+      if (item.children && item.children.length) {
+        groupChildAttr(item.children);
+      } else {
+        delete item.children;
+      }
+    }
+  })
+  return data;
+}
+
+function buildUnitsObject(unitList) {
+  const units = {};
+  (unitList || []).forEach(item => {
+    const splitItem = item.title.split('=');
+    if (splitItem.length) {
+      units[splitItem[0]] = splitItem[1];
+    }
+  });
+  return units;
+}
+
+function buildAttrUnit(unitMapper, data) {
+  data.map(item => {
+    if (item.title && isNumeric(item.title.split('=')[1])) {
+      if (item.title.toLowerCase().includes('diameter')) {
+        item.unit = unitMapper.diameterUnit;
+      }
+      if (item.title.toLowerCase().includes('elev')) {
+        item.unit = unitMapper.diameterUnit;
+      }
+      if (item.title.toLowerCase().includes('thickness') || item.title.toLowerCase().includes('width')) {
+        item.unit = unitMapper.widthUnit;
+      }
+      if (item.title.toLowerCase().includes('height')) {
+        item.unit = unitMapper.heightUnit;
+      }
+      if (item.title.toLowerCase().includes('volume') || item.title.toLowerCase().includes('area')) {
+        item.unit = unitMapper.areaUnit;
+      }
+
+    }
+    if (item.children) {
+      buildAttrUnit(unitMapper, item.children);
+    }
+  })
+  return data;
+}
+
+function isNumeric(str) {
+  if (typeof str != "string") return false
+  return !isNaN(str) &&
+    !isNaN(parseFloat(str))
+}
+
+module.exports = {
+  searchTreeNode,
+  traversalOTree,
+  shortenTree,
+  setTitleRoot,
+  searchTreeNodeForTowParents
+};
